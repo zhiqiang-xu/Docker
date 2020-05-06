4db70335159f23af7ddef0a596322fb336e97947
zhiqiang.xu <zhiqiang.xu@mercari.com>
2020-05-06 12:29:09 +0900

    modify main.go

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+  name, err := os.Hostname()
+  if err != nil {
+	panic(err)
+  }
+
+  fmt.Println("hostname:", name)
+  // fmt.Fprintf(w, "Hello, Kubernetes")
+}
+
+func main() {
+  http.HandleFunc("/", handler)
+  http.ListenAndServe(":8080", nil)
+}
